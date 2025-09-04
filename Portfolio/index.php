35e8c55d10cbeb2bf7e01ed937483120f0dcc1a1
Muzhahir <muzhahiryahaya@Gmail.com>
2025-09-04 21:49:37 +0800

    Add files via upload

---
+++ Portfolio/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="stylee.css">
+    <title>Portfolio Website</title>
+</head>
+<body>
+<header>
+    <a href="#" class="logo">Portfolio</a>
+        <nav>
+            <a href="index.php"class="active">Home</a>
+            <a href="project.php" >Projects</a>
+        
+
+        </nav>
+</header>
+    <section class="home">
+        
+        <div class="home-content">
+            <h1>Hi, I am <span>Muzhahir</span></h1>
+            <h3 class="typing-text" style="font-size: 3rem;">Focused in <span></span></h3>
+            <p>A Bachelor of Information Technology (Hons.) student in Universiti Teknologi MARA (UiTM) with hands-on experiences in design, development, testing of mobile app and web with a solid foundation in programming, database management, data analysis and UI/UX design. Additionally, awarded first place of best final year project for IT in UiTM Perlis. Demonstrated leadership as the president and assistant coach of a competitive marching band team, winning multiple national-level awards. Passionate about applying both technical and creative skills collaboratively to deliver impactful solutions and drive community engagement.</p>
+            <b><p>Contact: 011-3141 0225</p>
+            <p>Email: muzhahiryahaya@gmail.com</p></b>
+            <div class="social-icons">
+                <a href="www.linkedin.com/in/muhammad-muzhahir-bin-yahaya-08765720b"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/Muzhahir/"><i class="fa-brands fa-github"></i></a>
+                
+            </div>
+            <a href="#" class="btn">Hire me</a>
+        </div>
+        <div class="home-img">
+            <img src="images/M1.png" alt="">
+        </div>
+        
+    </section>
+    <section class="home-education" style="background-image: url('images/B1.png'); background-size: contain; background-position: center;">
+        <h1>EDUCATION</h1>
+
+        <div class="edu-grid">
+            <div class="edu-item">
+                <h2>SMK SUBANG BESTARI</h2>
+            <div class="institute-image" style="background-image: url('images/E1.jpg');"></div>
+                <p>Science and Architecture<br>SPM: 4A 4A- 1C+</p>
+            </div>
+            <div class="edu-item">
+                <h2>KOLEJ MATRIKULASI SELANGOR</h2>
+            <div class="institute-image" style="background-image: url('images/E2.jpg');"></div>
+                <p>Physical Science<br>PNGK: 3.58</p>
+            </div>
+            <div class="edu-item">
+                <h2>UiTM ARAU</h2>
+            <div class="institute-image" style="background-image: url('images/E3.jpg');"></div>
+                <p>Bachelor of Information Technology (Hons.)<br>Current CGPA: 3.17</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="extra-co">
+        <h1>EXTRA-CURRICULAR</h1>
+
+        <div class="extra-co-content">
+            <p>
+            I was active player in a competent marching band club as a drumline player. 
+            In the 2023/2024 season, I was appointed as the president of the club. I planned, 
+            collaborated, and led the team to achieve several more titles in the Malaysia World Band Competition (MWBC) 2023 and UiTM Marching Band Competition (UMBC) 2024.
+            Now, I am focusing on teaching and manage drumline team as a coach.
+            </p>
+        </div>
+
+        <div class="extra-co-gallery">
+            <img src="images/EC1.jpg" alt="Marching Band">
+            <img src="images/EC3.jpg" alt="Band Performance">
+            <img src="images/M5.jpg" alt="Rehearsal">            
+            <img src="images/EC4.jpg" alt="Me 2">
+            <img src="images/EC2.jpg" alt="DC Trophy">
+            <img src="images/EC6.jpg" alt="Award Ceremony">
+            <img src="images/EC5.jpg" alt="Marching Team">            
+            <img src="images/M6.jpg" alt="Me">
+
+        </div>
+
+            <div class="extra-bg">
+            <pre style="font-size: 1.5rem;">
+            Achievements:
+
+            • 1st place for marching percussion category in UiTM Marching Percussion 2023. 
+            
+            • 2nd place for marching street parade category in Malaysia World Band Competition (MWBC) 2023. 
+            
+            • 3rd place for marching field show category in Malaysia World Band Competition (MWBC) 2023. 
+
+            • 2nd place for snare solo category in UiTM Marching Band Competition (UMBC) 2024. 
+            
+            • 2nd place for marching percussion category in UiTM Marching Band Competition (UMBC) 2024. 
+            
+            • 3rd place for drumline battle in UiTM Marching Band Competition (UMBC) 2024. 
+            
+            • 2nd place for overall in UiTM Marching Band Competition (UMBC) 2024. 
+            </pre>
+            <div>
+
+    </section>
+
+</body>
+</html>
